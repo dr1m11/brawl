5548d7f11b5dd4fea545bfc02d1b4f14747b8f7e
dr1m11 <marat3389@gmail.com>
2024-11-15 01:33:33 +0300

    feat: crash update

--- src/components/Pages/Crash/components/History/History.tsx
+++ src/components/Pages/Crash/components/History/History.tsx
@@ -2,10 +2,24 @@
 import styles from './History.module.css'
 import localFont from "next/font/local";
 import clsx from "clsx";
-import {memo} from "react";
-import {useAppSelector} from "@/lib/hooks";
+import {memo, useEffect} from "react";
+import {useAppDispatch, useAppSelector} from "@/lib/hooks";
+import useWebsocket from "react-use-websocket";
+import {SOCKET_API_URL} from "@/constants";
+import {useQueryClient} from "@tanstack/react-query";
+import {setHistory, setIsBetSet, setSocketEvent, setUsersBets} from "@/lib/crashSlice/crashSlice";
+import {axiosClassic} from "@/api/axios";
 
 const daysOne = localFont({src: '../../../../../Fonts/DaysOne-Regular.ttf'});
+
+interface CrashInterface {
+    status: "Running" | "Crashed" | "Pending"
+    multiplier: number
+    time_before_start: number,
+    length: number,
+    rotate: number,
+    game_id: number,
+}
 
 const History = () => {
     function getColorByMultiplier(mult) {
@@ -23,6 +37,44 @@ const History = () => {
     }
 
     const history = useAppSelector(state => state.crash.history)
+
+    const {lastMessage, sendMessage, readyState} = useWebsocket(`${SOCKET_API_URL}/crash`)
+
+    const queryClient = useQueryClient()
+
+    const dispatch = useAppDispatch()
+
+    const user = useAppSelector(state => state.user.id)
+
+    const {
+        socketEvent,
+        isAutoBet,
+        isAutoWithdraw,
+        isBetSet,
+        usersBets,
+        bet
+    } = useAppSelector(state => state.crash)
+
+    useEffect(() => {
+        if (!lastMessage) return
+        const data = JSON.parse(lastMessage.data)
+        if (data.bets === null || data.bets) {
+            dispatch(setUsersBets(data.bets))
+        } else {
+            const mainData: CrashInterface = data
+            dispatch(setSocketEvent(mainData))
+        }
+    }, [lastMessage]);
+
+    useEffect(() => {
+        queryClient.invalidateQueries({
+            queryKey: ['user']
+        })
+        axiosClassic.get('/all-crash-records').then(data => dispatch(setHistory(data.data)))
+        if ((socketEvent.status === "Pending") && (!isAutoBet)) {
+            dispatch(setIsBetSet(false))
+        }
+    }, [socketEvent.status]);
 
     return (
         <div className={styles.history}>
