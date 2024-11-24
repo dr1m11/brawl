1e5fedab846c165cb52d11815adf8cfe63884191
dr1m11 <marat3389@gmail.com>
2024-11-25 00:21:27 +0300

    next 15

--- src/hooks/useSession.ts
+++
@@ -1,7 +0,0 @@
-import {getAccessToken} from "@/services/auth/auth.helper";
-
-export default function useSession() {
-    const isAuth = localStorage.getItem('isAuth') && getAccessToken()
-    const user = localStorage.getItem('user')
-    // const userBalance =
-}
