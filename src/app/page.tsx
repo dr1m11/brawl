f9fec094483be8d728b840d1d888bd9e1cc6b1f1
dr1m11 <marat3389@gmail.com>
2024-11-24 18:59:38 +0300

    yandex verify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import Head from "next/head";
 
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <meta name="yandex-verification" content="165730094b3146dc"/>
-      </Head>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </Head>
+            <HomePage/>
+        </>
+    );
 }
