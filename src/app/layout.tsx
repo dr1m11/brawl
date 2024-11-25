bb524a2dacf97bb87a186bf9b4c5e620ad8f0dfb
dr1m11 <marat3389@gmail.com>
2024-11-26 00:09:14 +0300

    feat: footer and header back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import Header from "@/components/Header/Header";
+import Header from "@/components/Header/Header";
 import clsx from "clsx";
-// import Footer from "@/components/Footer/Footer";
+import Footer from "@/components/Footer/Footer";
 import StoreProvider from "@/app/StoreProvider";
 import {QueryProvider} from "@/app/QueryProvider";
 import AuthWrapper from "@/components/AuthWrapper/AuthWrapper";
 import AuthProvider from "@/app/AuthProvider";
-// import {BottomMenu} from "@/components/BottomMenu/BottomMenu";
+import {BottomMenu} from "@/components/BottomMenu/BottomMenu";
 import {Toaster} from "react-hot-toast";
 
 const daysOne = localFont({src: '../Fonts/DaysOne-Regular.ttf'});
@@ -35,14 +35,14 @@ export default function RootLayout({
                     <html lang="en">
                     <body
                         className={clsx(daysOne.className, 'bg-brawl-purple-main h-full w-full overflow-x-hidden relative')}>
-                    {/*<Header/>*/}
+                    <Header/>
                     <main>
                         {children}
                         <AuthWrapper/>
                         <Toaster />
                     </main>
-                    {/*<Footer/>*/}
-                    {/*<BottomMenu/>*/}
+                    <Footer/>
+                    <BottomMenu/>
                     </body>
                     </html>
                 </AuthProvider>
