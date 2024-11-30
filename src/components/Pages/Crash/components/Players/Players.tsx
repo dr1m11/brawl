96ff81ae366bc8534590bc0ab873b36830282b70
dr1m11 <marat3389@gmail.com>
2024-12-01 01:50:13 +0300

    crash refactor

--- src/components/Pages/Crash/components/Players/Players.tsx
+++ src/components/Pages/Crash/components/Players/Players.tsx
@@ -1,7 +1,7 @@
 'use client'
 import PlayersList from "@/components/Pages/Crash/components/PlayersList/PlayersList";
 import styles from '../Kostil/Kostil.module.css'
-import {FC, memo, useMemo} from "react";
+import {FC, memo} from "react";
 import {TUsersBets} from "@/lib/crashSlice/crashUserBets";
 
 interface IProps{
@@ -11,13 +11,13 @@ interface IProps{
 
 const Players: FC<IProps> = ({crashUsersBets}) => {
     // Мемоизируйте длину для предотвращения лишних ре-рендеров
-    const betsCount = useMemo(() => crashUsersBets.length, [crashUsersBets]);
+    // const betsCount = useMemo(() => crashUsersBets.length, [crashUsersBets]);
 
     return (
         <>
             <PlayersList bets={crashUsersBets}/>
             <h5 className={styles.bets__count}>
-                Всего {betsCount} ставок
+                {/*Всего {betsCount} ставок*/}
             </h5>
         </>
     );
