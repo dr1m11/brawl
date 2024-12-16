ce4fac6ae2e30f692c46d5201a9a62011990c29e
dr1m11 <marat3389@gmail.com>
2024-12-16 04:06:33 +0300

    feat: crash update

--- src/components/Pages/Crash/components/PlayersList/PlayersList.tsx
+++ src/components/Pages/Crash/components/PlayersList/PlayersList.tsx
@@ -1,5 +1,3 @@
-import { FixedSizeList as List } from "react-window";
-import AutoSizer from "react-virtualized-auto-sizer";
 import styles from './PlayersList.module.css';
 import Player from "@/components/CrashPlayer/Player";
 
@@ -16,25 +14,6 @@ interface IProps {
 }
 
 const PlayersList = ({ bets }: IProps) => {
-    // Компонент строки списка
-    // eslint-disable-next-line react/display-name
-    const PlayerRow = ({ index, style }: { index: number, style: React.CSSProperties }) => {
-        const bet = bets[index];
-        return (
-            <div style={style}>
-                <Player
-                    key={`${bet.player_nickname}-${index}`}
-                    nickname={bet.player_nickname}
-                    amount={bet.amount}
-                    winning={bet.winning}
-                    multiplier={bet.user_multiplier}
-                    photo={bet.player_photo}
-                />
-            </div>
-        );
-    }
-
-    console.log('sdkhfg')
 
     return (
         <div
@@ -44,18 +23,18 @@ const PlayersList = ({ bets }: IProps) => {
                 width: '100%'
             }}
         >
-            <AutoSizer>
-                {({ height, width }) => (
-                    <List
-                        height={height}
-                        itemCount={bets.length}
-                        itemSize={60} // Высота элемента, подставьте реальную высоту вашего элемента
-                        width={width}
-                    >
-                        {PlayerRow}
-                    </List>
-                )}
-            </AutoSizer>
+            {
+                bets.map((bet) => (
+                    <Player
+                        key={bet.player_nickname}
+                        nickname={bet.player_nickname}
+                        amount={bet.amount}
+                        winning={bet.winning}
+                        multiplier={bet.user_multiplier}
+                        photo={bet.player_photo}
+                    />
+                ))
+            }
         </div>
     );
 };
