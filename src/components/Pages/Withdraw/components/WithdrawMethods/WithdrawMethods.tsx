dc2840ed7733a4a7e1d7b397c4e843fab1131461
dr1m11 <marat3389@gmail.com>
2024-12-21 17:08:48 +0300

    feat: payment page is adapted to mobile

--- src/components/Pages/Withdraw/components/WithdrawMethods/WithdrawMethods.tsx
+++ src/components/Pages/Withdraw/components/WithdrawMethods/WithdrawMethods.tsx
@@ -4,6 +4,7 @@ import WithdrawCard from "@/components/Pages/Withdraw/components/WithdrawCard/Wi
 import GemsCard from "@/components/Pages/Withdraw/components/GemsCard/GemsCard";
 import {useQuery} from "@tanstack/react-query";
 import {axiosClassic} from "@/api/axios";
+import {useAppSelector} from "@/lib/hooks";
 
 const WithdrawMethods = () => {
 
@@ -11,6 +12,8 @@ const WithdrawMethods = () => {
         queryKey: ['get-gems-price'],
         queryFn: () => axiosClassic.get('/gems-prices')
     })
+
+    const {isGameSelected} = useAppSelector(state => state.withdraw)
 
     
     return (
@@ -20,12 +23,14 @@ const WithdrawMethods = () => {
             </div>
             <div className={styles.gems}>
                 {
-                    isSuccess &&
+                    (isSuccess && isGameSelected) ?
                     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                     // @ts-expect-error
                     data.data.map((item) => (
                         <GemsCard key={item.id} position={item.position} img={item.photo} price={item.price}/>
                     ))
+                        :
+                        <p className={styles.alert}>Пожалуйста, выберите игру</p>
                 }
             </div>
         </div>
